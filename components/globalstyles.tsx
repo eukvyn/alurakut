2d25ff0c45f3780153b04ac00731c281c7bf11e1
Kevin Almeida <janioklalmeida05@gmail.com>
2023-03-23 00:47:11 -0300

    feat: setting global style

--- components/globalstyles.tsx
+++ components/globalstyles.tsx
@@ -1,13 +1,27 @@
 import { createGlobalStyle } from 'styled-components'
+import { AlurakutStyles } from '../lib/AlurakutCommons'
 
 const GlobalStyle = createGlobalStyle`
   html,
   body {
     color: ${({ theme }) => theme.colors.primary};
+    background-color: #D9E6F6;
     padding: 0;
     margin: 0;
     font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  #__next {
+    display: flex;
+    main-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
   }
 
   a {
@@ -18,6 +32,8 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
+
+  ${AlurakutStyles}
 `
 
 export default GlobalStyle
