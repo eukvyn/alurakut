c9c55efdf39b6b6e8fd5ad8811f1818f29bf4196
Kevin Almeida <janioklalmeida05@gmail.com>
2023-03-26 01:46:16 -0300

    feat: complete api bff create community

--- pages/api/communities.js
+++ pages/api/communities.js
@@ -1,7 +1,25 @@
-export default async function customRequest(request, response) {
+import { buildClient } from '@datocms/cma-client-node';
 
-    response.json({
+export default async function customRequest(request, response) {
+    
+    if (request.method === 'POST') {
         
+        const client = buildClient({apiToken: process.env.REACT_APP_API_TOKEN_DATO_FULL})
+    
+        const data = await client.items.create({
+            item_type: { type: 'item_type', id: '1244835' },
+            ...request.body
+        })
+    
+        response.json({
+            data: data
+        })
+        
+        return
+    }
+    
+    response.status(404).json({
+        'message': 'this endpoint is POST only'
     })
 
 }
