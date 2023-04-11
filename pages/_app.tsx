188c0628a26d1aa7619171d3af655c1db0e9a0e4
Kevin Almeida <janioklalmeida05@gmail.com>
2023-04-11 20:15:25 -0300

    chore: configure chakra-ui providers and themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,42 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider, DefaultTheme } from 'styled-components'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { Styles } from "@chakra-ui/theme-tools"
 import GlobalStyle from '../components/globalstyles'
 
-const theme: DefaultTheme = {
+const themeStyled: DefaultTheme = {
   colors: {
     primary: '#111',
     secondary: '#0070f3',
-  },
+  }
 }
+
+const colors = {
+  primary: '#111',
+  secondary: '#0070f3',
+}
+
+export const styles: Styles = {
+  global: {
+    body: {
+      bg: '#D9E6F6',
+    }
+  }
+}
+
+const theme = extendTheme({ 
+  colors,
+  styles
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <Component {...pageProps} />
+      <ThemeProvider theme={themeStyled}>
+        <ChakraProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ChakraProvider>
       </ThemeProvider>
     </>
   )
